9329a46239032825ee89b65f8df1903c01070ae9
SUBHANOk <117134323+SUBHANOk@users.noreply.github.com>
2022-11-03 15:14:35 +0500

    Add files via upload

---
+++ p.py
@@ -0,0 +1,32 @@
+import os, platform
+
+try:
+
+        import requests
+
+except:
+
+        os.system('pip2 install requests')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == "64bit":
+
+        from SUBHAN_64 import m
+
+        m()
+
+elif bit == "32bit":
+
+        from SUBHAN_32 import m
+
+        m()
+
+
+
+
+
+
+
