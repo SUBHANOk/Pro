11adb44bf80312e19987ed36a421b73e8621c2d6
SUBHANOk <117134323+SUBHANOk@users.noreply.github.com>
2022-11-03 15:17:06 +0500

    Update p.py

--- p.py
+++ p.py
@@ -14,13 +14,13 @@ bit = platform.architecture()[0]
 
 if bit == "64bit":
 
-        from SUBHAN_64 import m
+        from Pro_64 import 64
 
         m()
 
 elif bit == "32bit":
 
-        from SUBHAN_32 import m
+        from Pro_32 import 32
 
         m()
 
